a46b5056bbf08234419c68f05d68a472b758e95c
Derryel Anderson <derryelanderson@gmail.com>
2017-04-19 22:00:05 -0400

    set up public

--- meanapp/server.js
+++ meanapp/server.js
@@ -3,12 +3,16 @@ var app = express();
 var port = process.env.PORT || 8080;
 var morgan = require('morgan');
 var mongoose = require('mongoose');
-var User = require("./app/models/user");
 var bodyParser = require('body-parser');
+var router = express.Router();
+var appRoutes = require('./app/routes/api')(router);
+var path = require('path');
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use('/api', appRoutes);
 
 mongoose.connect('mongodb://localhost:27017/mean', function(err) {
 	if (err) {
@@ -19,23 +23,8 @@ mongoose.connect('mongodb://localhost:27017/mean', function(err) {
 });
 
 
-app.post('/users', function(req, res){
-	var user = new User();
-	user.username = req.body.username;
-	user.password = req.body.password;
-	user.email = req.body.email;
-	if (req.body.username == null || req.body.username == '' || req.body.password == null || req.body.password == '' || req.body.email == null || req.body.email == '' ) {
-		res.send('Ensure username, email, and password were provided');
-
-	} else {
-		user.save(function(err){
-		if (err) {
-			res.send('Username or Email already exist!');
-		} else {
-			res.send('user created');
-		}
-	});
-	}
+app.get('*', function(req, res){
+	res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
 });
 
 app.listen(port, function() {
